a5928ee96da1920b50cded27ca087e1320b6a72c
1Hanif1 <hanifmohammed869@gmail.com>
2021-10-24 17:08:36 +0500

    Final commit ig

--- JS/cart.js
+++ JS/cart.js
@@ -2,7 +2,11 @@ let pizzaJSON;
 let cartContainer;
 window.addEventListener('load', () => {
   getPizzaJSON();
-  getTotal();
+  const orderBtn = document.querySelector('.order--btn');
+  orderBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    placeOrder();
+  });
 });
 
 const getHttpRequest = function (method, file) {
@@ -23,6 +27,11 @@ const getPizzaJSON = function () {
   });
 };
 
+const updateQuantity = function (change, name, price = 0) {
+  let request = getHttpRequest('POST', './php/db.php');
+  request.send(`action=update&update=${change}&name=${name}&price=${price}`);
+};
+
 const getCartJson = function (pizzaJSON) {
   let request = getHttpRequest('POST', './php/db.php');
   request.send('action=cartjson');
@@ -31,6 +40,27 @@ const getCartJson = function (pizzaJSON) {
       let cartJSON = JSON.parse(this.responseText);
       renderCart(pizzaJSON, cartJSON);
     } catch (e) {}
+  });
+};
+
+const getTotal = function () {
+  const items = document.querySelectorAll('.item');
+  let total = 0;
+  items.forEach((item) => {
+    const price = parseInt(item.querySelector('.item__price').textContent);
+    const quantity = parseInt(item.querySelector('.quantity').textContent);
+    total += price * quantity;
+  });
+  const totalPrice = document.querySelector('.total--amount');
+  totalPrice.textContent = total;
+};
+
+const deleteItem = function (pizza, domElement) {
+  let request = getHttpRequest('POST', './php/db.php');
+  request.send(`action=delete&name=${pizza}`);
+  request.addEventListener('load', () => {
+    domElement.remove();
+    getTotal();
   });
 };
 
@@ -46,6 +76,7 @@ const renderCart = function (pizzas, cartItem) {
             src="${pizzas[pizza].image}"
             alt="/"
           />
+          <img src="./img/util/cancel.png" class="item__cancel--btn" alt="/" />
           <div class="item__details">
             <p class="item__name">${pizzas[pizza].name}</p>
             <div class="item__price--container">
@@ -75,29 +106,99 @@ const renderCart = function (pizzas, cartItem) {
     });
   });
 
+  getTotal();
+
   cartContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('item__cancel--btn')) {
+      const pizza = e.target
+        .closest('.item')
+        .querySelector('.item__name').textContent;
+      deleteItem(pizza, e.target.closest('.item'));
+    }
+
     if (e.target.classList.contains('left--arrow')) {
       const itemQuantity = e.target.closest('.item__quantity');
+      const pizza = itemQuantity
+        .closest('.item__details')
+        .querySelector('.item__name').textContent;
+      const price = itemQuantity
+        .closest('.item__details')
+        .querySelector('.item__price').textContent;
       let quantity = itemQuantity.querySelector('.quantity');
-      if (parseInt(quantity.textContent) >= 1) {
+      if (parseInt(quantity.textContent) > 1) {
         quantity.textContent = parseInt(quantity.textContent) - 1;
+        updateQuantity('decrease', pizza, price);
+        getTotal();
       }
     }
 
     if (e.target.classList.contains('right--arrow')) {
       const itemQuantity = e.target.closest('.item__quantity');
+      const pizza = itemQuantity
+        .closest('.item__details')
+        .querySelector('.item__name').textContent;
+      const price = itemQuantity
+        .closest('.item__details')
+        .querySelector('.item__price').textContent;
       let quantity = itemQuantity.querySelector('.quantity');
       quantity.textContent = parseInt(quantity.textContent) + 1;
+      updateQuantity('increase', pizza, price);
+      getTotal();
     }
   });
 };
 
-const getTotal = function () {
-  console.log('bruh');
+const showModal = function (str) {
+  const modal = document.querySelector('.modal');
+  modal.classList.remove('hidden');
+  const modalText = document.querySelector('.modal__text');
+  modalText.textContent = str;
+  const modalBtn = document.querySelector('.modal__btn');
+  modalBtn.addEventListener('click', function () {
+    modal.classList.add('hidden');
+  });
+};
+
+const clearCart = function () {
   let request = getHttpRequest('POST', './php/db.php');
-  request.send('action=getTotal');
+  request.send('action=clearcart');
+};
+
+const placeOrder = function () {
+  const totalPrice = document.querySelector('.total--amount');
+  if (parseInt(totalPrice.textContent) <= 0) {
+    showModal('🛒 Cart is empty');
+    return;
+  } // show cart is empty modal and return
+  // get form data
+  let name = document.querySelector('.order--name').value;
+  let contact = document.querySelector('.order--contact').value;
+  let address = document.querySelector('.order--address').value;
+  if ([name, contact, address].some((field) => field == '')) {
+    showModal('Please fill all details ⚠');
+    return;
+  } // Show modal 'please fill all details'
+  // make ajax request to php
+  let pizzas = '';
+  const cartItems = document.querySelector('.left').querySelectorAll('.item');
+  cartItems.forEach((item) => {
+    const name = item.querySelector('.item__name').textContent.trim();
+    const quantity = item.querySelector('.item__quantity').textContent.trim();
+    pizzas += `${quantity} ${name}, `;
+  });
+  console.log(pizzas.trim());
+  let total = parseInt(document.querySelector('.total--amount').textContent);
+
+  let request = getHttpRequest('POST', './php/db.php');
+  request.send(
+    `action=order&total=${total}&pizzas=${pizzas}&cname=${name}&contact=${contact}&address=${address}`
+  );
   request.addEventListener('load', function () {
-    const total = document.querySelector('.total--amount');
-    total.textContent = JSON.parse(this.responseText)[0]['SUM(`price`)'];
+    if (this.responseText == 'true') {
+      clearCart();
+      showModal('Order placed successfully 🥳');
+      document.querySelector('.left').innerHTML = '';
+      getTotal();
+    } else showModal('Failed to place order 😟');
   });
 };
--- php/db.php
+++ php/db.php
@@ -32,26 +32,88 @@ function getCartData(){
     } catch(Exception $e) {echo "Exception".$e;}
 }
 
-function getTotal(){
-    try {
+// function getTotal(){
+//     try {
+//         $connection = new mysqli("localhost","root","","barizza");
+//         $sql = "SELECT SUM(`price`) FROM `cart`";
+//         if($connection->query($sql)->num_rows > 0) {
+//             $result = $connection->query($sql);
+//             $totalArray = array();
+//             while($row =mysqli_fetch_assoc($result))
+//             {$totalarray[] = $row;}
+//             echo json_encode($totalarray);
+//         }
+//     } catch(Exception $e){
+//         echo $e;
+//     }
+// }
+
+function update(){
+    try{
         $connection = new mysqli("localhost","root","","barizza");
-        $sql = "SELECT SUM(`price`) FROM `cart`";
+        $sql = "SELECT * FROM `cart` WHERE `name`='".$_POST['name']."'";
+        
         if($connection->query($sql)->num_rows > 0) {
-            $result = $connection->query($sql);
-            $totalArray = array();
-            while($row =mysqli_fetch_assoc($result))
-            {$totalarray[] = $row;}
-            echo json_encode($totalarray);
-        }
-    } catch(Exception $e){
-        echo $e;
+            switch($_POST['update']){
+                case 'increase': {
+                  $sql = "UPDATE `cart` SET `quantity`= `quantity` + 1,`price`= `price` + ".$_POST['price']." WHERE `name`='".$_POST['name']."'";
+                  $connection->query($sql);
+                  break;  
+                }
+                case 'decrease':{
+                    $sql = "UPDATE `cart` SET `quantity`= `quantity` - 1,`price`= `price` - ".$_POST['price']." WHERE `name`='".$_POST['name']."'";
+                    $connection->query($sql);
+                }
+            }
+        } 
+    } catch (Exception $e) {
+        echo "Exception: ".$e;
+    }
+}
+
+function remove(){
+        try{
+        $connection = new mysqli("localhost","root","","barizza");
+        $sql = "DELETE FROM `cart` WHERE `name`='".$_POST['name']."'";
+        $connection->query($sql);
+    } catch (Exception $e) {
+        echo "Exception: ".$e;
+    }
+}
+
+function storeOrder(){
+    try{
+        $connection = new mysqli("localhost","root","","barizza");
+        $sql = "INSERT INTO `orders` (`total`,`pizzas`,`cname`,`contact`,`address`) VALUES (".$_POST['total'].",'".$_POST['pizzas']."','".$_POST['cname']."',".$_POST['contact'].",'".$_POST['address']."')";
+        if($connection->query($sql)) echo "true";
+        else echo "false";
+    } catch (Exception $e) {
+        echo "Exception: ".$e;
+    }
+}
+
+function clearCart(){
+        try{
+        $connection = new mysqli("localhost","root","","barizza");
+        $sql = "DELETE FROM `cart`";
+        if($connection->query($sql)) echo "true";
+        else echo "false";
+    } catch (Exception $e) {
+        echo "Exception: ".$e;
     }
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if($_POST['action'] == 'store') storeData();   
-    if($_POST['action'] == 'json') echo file_get_contents("../JSON/pizzas.json");
-    if($_POST['action'] == 'cartjson') getCartData();
-    if($_POST['action'] == 'getTotal') getTotal();
+    switch($_POST['action']){
+        case 'store': storeData(); break;
+        case 'json': echo file_get_contents("../JSON/pizzas.json"); break;
+        case 'cartjson': getCartData(); break;
+        // case 'getTotal': getTotal(); break;
+        case 'update': update(); break;
+        case 'delete': remove(); break;
+        case 'order': storeOrder(); break;
+        case 'clearcart': clearCart(); break;
+    }
+
 }
 ?>
